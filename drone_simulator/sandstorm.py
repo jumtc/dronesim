1fa49838074adcf2844adda8e4b916b20edbaafc
mr_priyam <priyam.juee@gmail.com>
2025-04-02 23:59:12 +0530

    removing random gyroscopic values

--- drone_simulator/sandstorm.py
+++ drone_simulator/sandstorm.py
@@ -7,7 +7,7 @@ class Sandstorm:
         self.windspeed = random.uniform(15, 25)  # Moderate sandstorm windspeed is usually 15-25 in m/s
 
     def apply_effect(self, gyroscope):
-        wind_direction = random.choice([-1, 1])  # -1 for backward, 1 for forward
+        wind_direction = random.choice([-1, 1])  # -1 for forward wind , 1 for backward wind
         # Calculate pitch disturbance based on windspeed and direction
         self.pitch_disturbance = wind_direction * np.interp(self.windspeed, [15, 25], [0, 1]) #pitch disturbance -1 to 1
         gyroscope[1] += self.pitch_disturbance  # Modify the pitch value
